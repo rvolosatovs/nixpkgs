dc3a9ccdfb52c821bfb62b17b7fb32a8bbc31dfa
Roman Volosatovs <rvolosatovs@riseup.net>
2020-02-01 16:47:03 +0100

    Revert "python-miio: init at 0.4.7"
    This reverts commit d31217eb5743dbe3377206f6799f25d75a0da7d7.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10672,8 +10672,6 @@ in
 
   pyrseas = callPackage ../development/tools/database/pyrseas { };
 
-  python-miio = callPackage ../development/tools/python-miio { };
-
   qtcreator = libsForQt5.callPackage ../development/tools/qtcreator { };
 
   qxmledit = libsForQt5.callPackage ../applications/editors/qxmledit {} ;
