fc3a1206697092555f8c40de226bfd3357bbb23d
AndersonTorres <torres.anderson.85@protonmail.com>
2021-01-20 21:04:09 -0300

    qmk_cli: init at 0.0.39

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31728,6 +31728,7 @@ in
 
   pentablet-driver = libsForQt5.callPackage ../misc/drivers/pentablet-driver { };
 
+  qmk_cli = python3Packages.callPackage ../development/misc/qmk/cli.nix { };
   qmk_firmware = callPackage ../development/misc/qmk/firmware.nix {
     avrgcc = pkgsCross.avr.buildPackages.gcc;
     avrbinutils = pkgsCross.avr.buildPackages.binutils;
@@ -31740,7 +31741,7 @@ in
   newlib = callPackage ../development/misc/newlib { };
   newlibCross = callPackage ../development/misc/newlib {
     stdenv = crossLibcStdenv;
-    };
+  };
 
   omnisharp-roslyn = callPackage ../development/tools/omnisharp-roslyn { };
 
