14baaa239977efacd9a0afcb359eabbcf05bcdaf
Roman Volosatovs <rvolosatovs@riseup.net>
2022-04-30 03:02:27 +0200

    Revert "quake3wrapper: Use quake3e by default"
    This reverts commit 923548a0fb7aec5495d9e6ff98cbce648e080614.

--- pkgs/games/quake3/wrapper/default.nix
+++ pkgs/games/quake3/wrapper/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildEnv, lib, libGL, quake3e, makeWrapper }:
+{ stdenv, buildEnv, lib, libGL, ioquake3, makeWrapper }:
 
 { paks, name ? (lib.head paks).name, description ? "" }:
 
@@ -6,11 +6,11 @@ let
   libPath = lib.makeLibraryPath [ libGL stdenv.cc.cc ];
   env = buildEnv {
     name = "quake3-env";
-    paths = [ quake3e ] ++ paks;
+    paths = [ ioquake3 ] ++ paks;
   };
 
 in stdenv.mkDerivation {
-  name = "${name}-${quake3e.name}";
+  name = "${name}-${ioquake3.name}";
 
   nativeBuildInputs = [ makeWrapper ];
 
@@ -20,11 +20,11 @@ in stdenv.mkDerivation {
     # We add Mesa to the end of $LD_LIBRARY_PATH to provide fallback
     # software rendering. GCC is needed so that libgcc_s.so can be found
     # when Mesa is used.
-    makeWrapper ${env}/bin/quake3e.x64 $out/bin/quake3 \
+    makeWrapper ${env}/ioquake3.* $out/bin/quake3 \
       --suffix-each LD_LIBRARY_PATH ':' "${libPath}" \
       --add-flags "+set fs_basepath ${env} +set r_allowSoftwareGL 1"
 
-    makeWrapper ${env}/bin/quake3e.ded.x64 $out/bin/quake3-server \
+    makeWrapper ${env}/ioq3ded.* $out/bin/quake3-server \
       --add-flags "+set fs_basepath ${env}"
   '';
 
