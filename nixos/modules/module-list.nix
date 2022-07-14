2eed1e6d2de204665e04e5475425aa94486f0461
Roman Volosatovs <rvolosatovs@riseup.net>
2022-07-14 15:09:21 +0200

    nixos/enarx: init service module
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -973,6 +973,7 @@
   ./services/security/certmgr.nix
   ./services/security/cfssl.nix
   ./services/security/clamav.nix
+  ./services/security/enarx.nix
   ./services/security/fail2ban.nix
   ./services/security/fprintd.nix
   ./services/security/haka.nix
